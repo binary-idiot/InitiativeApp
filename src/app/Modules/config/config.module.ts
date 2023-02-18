41e6ef2813060eb4d2ab70ad55ab7ba203cb2f51
Jonah Middleton <jonah.middleton@gmail.com>
2023-02-18 01:30:55 -0500

    Add ConfigPage

---
+++ src/app/Modules/config/config.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConfigPage } from './pages/config-page/config-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    ConfigPage
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ConfigModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {TrackerModule} from "@modules/tracker/tracker.module";
+import {ConfigModule} from "@modules/config/config.module";
 import {TrackerPage} from "@modules/tracker/pages/tracker-page/tracker-page.component";
+import {ConfigPage} from "@modules/config/pages/config-page/config-page.component";
+import {TrackerModule} from "@modules/tracker/tracker.module";
 
 const routes: Routes = [
-  { path: '', component: TrackerPage}
+  { path: 'tracker', component: TrackerPage },
+  { path: 'config', component: ConfigPage },
+  { path: '', redirectTo: 'tracker', pathMatch: 'full' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [
     RouterModule,
-    TrackerModule
+    TrackerModule,
+    ConfigModule
   ]
 })
 export class AppRoutingModule { }
