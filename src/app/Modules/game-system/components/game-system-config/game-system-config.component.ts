b76483b3a82134a7802d3bd31cbf0085d8119387
Jonah Middleton <jonah.middleton@gmail.com>
2023-03-08 00:39:10 -0500

    Build formgroup for GameSystemConfigComponent

--- src/app/Modules/game-system/components/game-system-config/game-system-config.component.ts
+++ src/app/Modules/game-system/components/game-system-config/game-system-config.component.ts
@@ -4,6 +4,8 @@ import {Observable, of, Subscription, switchMap, take} from "rxjs";
 import {GameSystem} from "@shared/models/game-system.model";
 import {GameSystemFacade} from "@modules/game-system/game-system.facade";
 import {nameOf} from "@shared/utils/type-utils";
+import { FormBuilder } from "@angular/forms";
+import {ItemDataFieldSchema} from "@shared/models/item.schema";
 
 @Component({
   selector: 'app-game-system-config',
@@ -15,16 +17,19 @@ export class GameSystemConfigComponent implements OnInit, OnDestroy {
   systemSub: Subscription;
   systems: GameSystem[];
   selectedSystem$: Observable<GameSystem | null>;
+
+  systemForm: unknown;
   constructor(
     public systemService: GameSystemService,
-    public systemFacade: GameSystemFacade) {
+    public systemFacade: GameSystemFacade,
+    private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
     this.systemSub = this.systemFacade.systems$
       .subscribe((systems: GameSystem[]) => {
-        this.systems = systems ?? [];
-        //this.systems.push(this.systemService.getNewSystem());
+        this.systems = [...systems] ?? [];
+        this.systems.push(this.systemService.getNewSystem());
     });
 
     this.selectedSystem$ = this.systemFacade.selectedSystem$
@@ -34,6 +39,33 @@ export class GameSystemConfigComponent implements OnInit, OnDestroy {
           return of(system);
         })
       )
+  }
+
+  buildForm(system: GameSystem): void {
+    const dataFields = system.itemModel.dataFields
+      .map((df: ItemDataFieldSchema) => this.fb.nonNullable.group({
+        key: df.key,
+        label: df.label,
+        type: df.type,
+        defaultValue: df.defaultValue,
+        required: df.required
+      }))
+
+    this.systemForm = this.fb.nonNullable.group({
+      key: system.key,
+      name: system.name,
+      itemModel: this.fb.nonNullable.group({
+        nameField: this.fb.nonNullable.group({
+          label: system.itemModel.nameField.label,
+          defaultValue: system.itemModel.nameField.defaultValue
+        }),
+        orderField: this.fb.nonNullable.group({
+          label: system.itemModel.orderField.label,
+          defaultValue: system.itemModel.orderField.defaultValue
+        }),
+        dataFields: dataFields
+      })
+    })
   }
 
   ngOnDestroy(): void {
